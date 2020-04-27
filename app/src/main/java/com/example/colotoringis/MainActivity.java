cef78a59f15b8596c4c058957570b27cde61aaf7
Артем <kudinovartem65@gmail.com>
2020-04-27 17:38:36 +0400

    21
    Signed-off-by: Артем <kudinovartem65@gmail.com>

---
+++ app/src/main/java/com/example/colotoringis/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.colotoringis;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private class coloring {
+        Coloring coloring;
+        private String color;
+
+        public void getColor() {
+
+        }
+
+        public void getOnCreate() {
+            Bitmap bitmap;
+
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+1
+}
